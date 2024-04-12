4b4a82ebec8a11d851434a63faeecb8424c86c98
yejinlong <415571973@qq.com>
2024-04-12 14:22:19 +0800

    sn tool

---
+++ sntool/SnToolDlg.h
@@ -0,0 +1,37 @@
+﻿
+// SnToolDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CSnToolDlg 对话框
+class CSnToolDlg : public CDialogEx
+{
+// 构造
+public:
+	CSnToolDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SNTOOL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CEdit m_editSn;
+	afx_msg void OnBnClickedButtonCreate();
+};
